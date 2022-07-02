5717530699aff44f1f48970c40e05e7ab0f6ce5a
kunj <kunj.koladiya1972@gmail.com>
2022-07-02 03:07:37 -0700

    commit

---
+++ bank2.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+class bank
+{
+    int accno,bal;
+    string name;
+    public:
+    bank(int accno,int bal,string name)
+    {
+        this->accno=accno;
+        this->bal=bal;
+        this->name=name;
+
+    }
+    void getdata()
+    {
+        cout<<"\n name is:"<<name;
+        cout<<"\t account no:"<<accno;
+        cout<<"\t current balance is:"<<bal;
+    }
+};
+int main()
+{
+    bank a(100,1100,"KUNJ KOLADIYA"),b(101,10200,"HET PATEL");
+    a.getdata();
+    b.getdata();
+    return 0;
+}
--- prc1.cpp
+++ prc1.cpp
@@ -6,5 +6,4 @@ int main()
     std::cout<<"\t b: "<<b;
     std::cout<<"\t a+b: "<<a+b;
     std::cout<<"\n hello...";
-
 }
